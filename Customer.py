7504d0ed27398ac5995dce5c2f7b31e167a04d3e
Reginawambui <ngendoanita8@gmail.com>
2023-12-17 20:16:04 +0300

    created classes for customer and restaurant

---
+++ Customer.py
@@ -0,0 +1,9 @@
+class Customer :
+    def __init__(self,name) :
+        self.name = name 
+
+
+ Regina wambui= Customer ("Regina wambui")
+  
+
+        
---
+++ Restaurant.py
@@ -0,0 +1,6 @@
+class Restaurant:
+    def __init__(self,name) :
+        self.name = name
+        pass
+    Happycafe = Restaurant ("Happycafe")
+    
